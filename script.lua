e35a1e8238c9a321a8c4817a09249a1a9a637344
Amiriki <73649897+Amiriki@users.noreply.github.com>
2023-08-22 14:21:59 +0100

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,20 +1,3 @@
--- Script Config
-
-getgenv().FOFConfig = {
-	['AutofarmEnabled'] = true,
-	['NPCWeapon'] = 'Battle Cleaver', -- The name of the weapon that you want to use to clear the NPCs. An axe is recommended.
-	['BossWeapon'] = 'Greatsword of Flying II', -- The name of the weapon that will be used to kill the general once the other enemies are dead.
-	['WebhookURL'] = 'https://discord.com/api/webhooks/1137281078210600971/m_tLqM1czjsznCrbN8FAfdb7pYbf_l13XBixiWn-lVb6IDlr-LoFCuEx-VEmpQojOTAr',
-	['DisableOnJoin'] = true, -- Whether you want the script to automatically disable itself if someone joins the server.
-	['EnableOnLeave'] = true, -- Whether you want the script to automatically restart when you are the only person in the server.
-	['IgnorePlayers'] = false, -- Whether the script should ignore players on the enemy team. This is useful for farming on two accounts at once.
-	['Map'] = 'Savannah', -- Which map the script should vote for.
-	['DisableRendering'] = false, -- Whether you want to disable rendering.
-	['AutoShutdownTimer'] = 6, -- How long the script should autofarm in hours before shutting down in order to seem legit. Set it to 0 to not automatically shutdown.
-	['AttackNeutralNPCs'] = true, -- Whether the script should attack Neutral NPCs, e.g. Rogues
-	['UseGeneralWeaponNPCs'] = {["mage"] = true, ["archer"] = true} -- What enemies (aside from the General) the script should use your General-killing weapon on. This is useful for spread out enemies like mages and archers.
-}
-
 -- Script Variables
 
 local Players = game:GetService('Players')
